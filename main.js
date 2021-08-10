cea3a93fcd68e808c8e95fd2b0b9a04d836599d1
xJosee <3006762980101@ingenieria.usac.edu.gt>
2021-08-10 16:17:13 -0600

    [fix] optimize operation method

--- main.js
+++ main.js
@@ -4,38 +4,29 @@ const $ = (selector) => document.querySelector(selector);
 const $btns = $All('.button');
 const $result = $('.results');
 
-var result = '';
+let values = Array(3);
+let result = '';
 
-let operators = {
+const operators = {
     '+': (num1, num2) => Number(num1) + Number(num2),
     '-': (num1, num2) => Number(num1) - Number(num2),
     '/': (num1, num2) => Number(num1) / Number(num2),
     'x': (num1, num2) => Number(num1) * Number(num2),
 };
 
-let operator = null;
-let temp = null;
-
 $btns.forEach(($btn) => {
-    $btn.addEventListener(('click'), operation);
+    $btn.addEventListener('click', operation);
 });
 
 function operation(e) {
-    let value = e.target.textContent;
+    let value = e.target.textContent; // [num || operator || del || reset || '='];
 
-    if (value == 'del' || value == 'reset') {
-        result = '';
-        temp = '';
-        operator = '';
-    }
-    //verifica si es un operador
-    else if (operators[value]) {
-        operator = value;
-        temp = Number(result);
-        result = '';
-    }
-    // se efectura la operacion
-    else if (value == '=' && !isNaN(temp) && !isNaN(result)) result = operators[operator](temp, result);
+    if (value == 'del' || value == 'reset') result = '';
+    // verifica que venga '=' y que los valores a operar sean numeros
+    if (value == '=' && !isNaN(values[0]) && !isNaN(result)) result = operators[values[1]](values[0], result);
+    //verifica si es un operador y guarda el primer operador y el operador
+    if (operators[value]) [values[1], values[0], result] = [value, Number(result), ''];
+    //concatena los numeros
     else result += value;
 
     if (!isNaN(result)) $result.textContent = result;
